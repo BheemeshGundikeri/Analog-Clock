320da303df75c9c70d88b0cca5473cabdf2d2a4f
BheemeshGundikeri <bheemesh.gundikeri24@gmail.com>
2025-03-06 09:52:05 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'clock-app';
+export class AppComponent implements OnInit {
+
+  hourDeg: number = 0;
+  minuteDeg: number = 0;
+  secondDeg: number = 0;
+  clockNumbers: { value: number; angle: number }[] = [];
+
+  ngOnInit() {
+    this.initializeClockNumbers();
+    this.animateClock();
+  }
+
+  animateClock() {
+    const now = new Date();
+    const milliseconds = now.getMilliseconds();
+    const seconds = now.getSeconds() + milliseconds / 1000; // Smooth movement
+
+    this.secondDeg = seconds * 6; // 360° / 60 sec
+    this.minuteDeg = now.getMinutes() * 6 + seconds * 0.1; // Smooth minute hand
+    this.hourDeg = (now.getHours() % 12) * 30 + now.getMinutes() * 0.5;
+
+    requestAnimationFrame(() => this.animateClock()); // Runs smoothly every frame
+    throw new Error('Method not implemented.');
+  }
+
+  initializeClockNumbers() {
+    for (let i = 1; i <= 12; i++) {
+      this.clockNumbers.push({ value: i, angle: i * 30 });
+    }
+  }
 }
